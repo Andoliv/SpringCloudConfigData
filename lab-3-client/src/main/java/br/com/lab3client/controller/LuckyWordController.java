9bc40558268c5425df8020974e03dd55987664e8
Anderson Oliveira <andoliv@gmail.com>
2016-02-18 13:25:58 -0200

    Adicionados os projetos do Eclipse

---
+++ lab-3-client/src/main/java/br/com/lab3client/controller/LuckyWordController.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package br.com.lab3client.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 
+ * @author anderson.oliveira
+ *
+ */
+@RestController
+public class LuckyWordController {
+
+	@Value("${lucky-word}")	String luckyWord;
+
+	@RequestMapping("/lucky-word")
+	public String showLuckyWord() {
+		return "The lucky word is: " + luckyWord;
+	}
+}
